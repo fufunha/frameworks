de6f337edfb3f49a657098ebbf80ee3d190a14fd
fufunha <maria.mendes10@estudante.ifms.edu.br>
2026-03-04 12:18:07 -0400

    fandangos

---
+++ src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,19 @@
+import { lchown } from "fs";
+
+export class CreateTodoDto {
+    title:       String;
+  decription:      String;
+  completed  :    Boolean;
+  priority:    TodoPriority; 
+  dueAt:        Date;
+  completedAt:  Date;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+enum TodoPriority {
+    LOW = 'LOW'
+    MEDIUM='MEDIUM'
+    HIGH ='HIGH'
+}
